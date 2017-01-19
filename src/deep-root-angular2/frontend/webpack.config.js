46f8a445fa3a717b6cfac5bc9047a3590f134b63
mgoria <mgoria@mitocgroup.com>
2017-01-19 17:39:34 +0200

    Fix css loader

--- src/deep-root-angular2/frontend/webpack.config.js
+++ src/deep-root-angular2/frontend/webpack.config.js
@@ -4,73 +4,78 @@ var path = require('path');
 var helpers = require('./config/helpers');
 
 var webpackConfig = webpackMerge(helpers.webpackDepsConfig(), {
-  context: helpers.root('..', '..'),
-  entry: {
-    'bootstrap': helpers.root('bootstrap.ts'),
-    'js/vendor': helpers.root('js/vendor.ts'),
-    'js/polyfills': helpers.root('js/polyfills.ts'),
-  },
+    context: helpers.root('..', '..'),
+    entry: {
+      'bootstrap': helpers.root('bootstrap.ts'),
+      'js/vendor': helpers.root('js/vendor.ts'),
+      'js/polyfills': helpers.root('js/polyfills.ts'),
+    },
 
-  resolve: {
-    modules: [
-      helpers.root('node_modules'),].concat(
-      helpers.getMicroservices().map(ms => path.join(ms, 'frontend', 'node_modules'))
-    ),
-    extensions: ['.js', '.es6', '.ts', '.json'],
-  },
+    resolve: {
+      modules: [
+        helpers.root('node_modules'),
+      ].concat(
+        helpers.getMicroservices().map(ms => path.join(ms, 'frontend', 'node_modules'))
+  ),
+  extensions: ['.js', '.es6', '.ts', '.json', '.css'],
+},
 
-  get resolveLoader() {
-    return this.resolve;
-  },
+get resolveLoader() {
+  return this.resolve;
+},
 
-  module: {
+module: {
+  loaders: [{
+    test: /\.ts$/,
     loaders: [
-      {
-        test: /\.ts$/,
-        loaders: [
-          'awesome-typescript-loader',
-          'angular2-template-loader',
-        ]
-      },
-      {
-        test: /\.html$/,
-        loader: 'raw-loader',
-        exclude: [helpers.root('index.html')],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file',
-        query: {
-          name: 'assets/[name].[hash].[ext]'
-        }
-      },
-      {
-        test: /\.css$/,
-        loader: 'raw-loader'
-      },
-      {
-        test: /\.es6$/,
-        loader: 'babel',
-        exclude: /(node_modules|bower_components|dist)/i,
-      },
-      {
-        test: /\.json$/,
-        loader: 'json',
-      },
-    ],
+      'awesome-typescript-loader',
+      'angular2-template-loader',
+    ]
   },
-
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['js/vendor', 'js/polyfills'],
-    }),
+    {
+      test: /\.html$/,
+      loader: 'raw-loader',
+      exclude: [helpers.root('index.html')],
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+      loader: 'file',
+      query: {
+        name: 'assets/[name].[hash].[ext]'
+      }
+    },
+    {
+      test: /\.css$/,
+      loader: 'raw-loader',
+      exclude: /@angular\/material\/core\/theming\/.+/i,
+    },
+    {
+      test: /@angular\/material\/core\/theming\/.+\.css$/i,
+      loaders: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.es6$/,
+      loader: 'babel',
+      exclude: /(node_modules|bower_components|dist)/i,
+    },
+    {
+      test: /\.json$/,
+      loader: 'json',
+    },
   ],
+},
+
+plugins: [
+  new webpack.optimize.CommonsChunkPlugin({
+    name: ['js/vendor', 'js/polyfills'],
+  }),
+],
 
   output: {
-    path: helpers.root(),
+  path: helpers.root(),
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
-  },
+},
 });
 
 if (['dev', 'stage'].indexOf(helpers.deeployConfig().env) !== -1) {
