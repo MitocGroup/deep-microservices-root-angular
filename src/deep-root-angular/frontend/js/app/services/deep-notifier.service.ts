e793828f6222fc5eccd4d2b695b3cd521573f8dd
grigoreme <grigore997@gmail.com>
2017-08-02 13:56:35 +0300

    Fix notifications render

--- src/deep-root-angular/frontend/js/app/services/deep-notifier.service.ts
+++ src/deep-root-angular/frontend/js/app/services/deep-notifier.service.ts
@@ -2,12 +2,21 @@
  * Created by mgoria on 12/09/16.
  */
 
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { NotificationsService } from 'angular2-notifications';
+
+interface MsgLayout {
+  type: string,
+  title: string,
+  message: string,
+  options: any,
+}
 
 @Injectable()
 export class DeepNotifierService {
-  private deepLogger : any = null;
+  private deepLogger: any = null;
+  private notifications: any[] = [];
+  private ready: boolean = false;
 
   /**
    * @param {NotificationsService} notifier
@@ -18,13 +27,27 @@ export class DeepNotifierService {
       null;
   }
 
+  setAsReady() {
+    this.ready = true;
+    this.showDelayedNotifications();
+  }
+
   /**
    * @param {String} title
    * @param {String} message
    * @param {*} options
    */
   success(title, message, options = null) {
-    this.notifier.success(title, message, options);
+    this.notifications.push({
+      type: 'success',
+      title: title,
+      message: message,
+      options: options,
+    });
+
+    if (this.ready) {
+      this.showDelayedNotifications();
+    }
   }
 
   /**
@@ -33,10 +56,15 @@ export class DeepNotifierService {
    * @param {*} options
    */
   error(title, message, options = null) {
-    this.notifier.error(title, message, options);
+    this.notifications.push({
+      type: 'error',
+      title: title,
+      message: message,
+      options: options,
+    });
 
-    if (this.deepLogger) {
-      this.deepLogger.err(`${title}. ${message}`);
+    if (this.ready) {
+      this.showDelayedNotifications();
     }
   }
 
@@ -46,10 +74,15 @@ export class DeepNotifierService {
    * @param {*} options
    */
   alert(title, message, options = null) {
-    this.notifier.error(title, message, options);
+    this.notifications.push({
+      type: 'alert',
+      title: title,
+      message: message,
+      options: options,
+    });
 
-    if (this.deepLogger) {
-      this.deepLogger.err(`${title}. ${message}`);
+    if (this.ready) {
+      this.showDelayedNotifications();
     }
   }
 
@@ -59,6 +92,82 @@ export class DeepNotifierService {
    * @param {*} options
    */
   info(title, message, options = null) {
-    this.notifier.info(title, message, options);
+    this.notifications.push({
+      type: 'info',
+      title: title,
+      message: message,
+      options: options,
+    });
+
+    if (this.ready) {
+      this.showDelayedNotifications();
+    }
   }
+
+  private showDelayedNotifications() {
+    this.notifications
+      .forEach((msg: MsgLayout, index, object) => {
+        msg.options = msg.options || [];
+
+        this.callRightMethod(msg);
+        object.splice(index, 1);
+      });
+  }
+
+  private callRightMethod(msg: MsgLayout) {
+    if (!msg) {
+      return;
+    }
+
+    if (msg.type === 'error') {
+      this.callError(msg);
+    } else if (msg.type === 'info') {
+      this.callInfo(msg);
+    } else if (msg.type === 'alert') {
+      this.callAlert(msg);
+    } else if (msg.type === 'success') {
+      this.callSuccess(msg);
+    }
+  }
+
+  private callError(msg: MsgLayout) {
+    this.notifier.error(
+      msg.title,
+      msg.message,
+      msg.options,
+    );
+
+    if (this.deepLogger) {
+      this.deepLogger.err(`${msg.title}. ${msg.message}`);
+    }
+  }
+
+  private callInfo(msg: MsgLayout) {
+    this.notifier.info(
+      msg.title,
+      msg.message,
+      msg.options,
+    );
+  }
+
+  private callSuccess(msg: MsgLayout) {
+    this.notifier.success(
+      msg.title,
+      msg.message,
+      msg.options,
+    );
+  }
+
+  private callAlert(msg: MsgLayout) {
+    this.notifier.alert(
+      msg.title,
+      msg.message,
+      msg.options,
+    );
+
+    if (this.deepLogger) {
+      this.deepLogger.err(`${msg.title}. ${msg.message}`);
+    }
+  }
+
 }
